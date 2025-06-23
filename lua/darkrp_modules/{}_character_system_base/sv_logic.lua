77267348aade0ed0690571a7c3cfd78762303a85
chuwurny <chuwurny@national.shitposting.agency>
2025-06-23 21:34:51 +0300

    refactor: separate sv_init into two files

--- lua/darkrp_modules/{}_character_system_base/sv_logic.lua
+++ lua/darkrp_modules/{}_character_system_base/sv_logic.lua
@@ -221,33 +221,3 @@ hook.Add("playerGetSalary", "DarkRPCharacters_NoCharNoSalary", function(ply)
         return false, nil, false
     end
 end)
-
-concommand.Add("darkrp_wipe_chars", function(ply)
-    if IsValid(ply) then
-        return
-    end
-
-    MySQLite.query(
-        [[DELETE FROM darkrp_characters;
-          DELETE FROM darkrp_chars_pos;]],
-        nil,
-        DarkRP.Characters._TraceAsyncError()
-    )
-
-    print("Characters has been wiped!")
-end)
-
-concommand.Add("darkrp_drop_chars", function(ply)
-    if IsValid(ply) then
-        return
-    end
-
-    MySQLite.query(
-        [[DROP TABLE darkrp_characters;
-          DROP TABLE darkrp_chars_pos;]],
-        nil,
-        DarkRP.Characters._TraceAsyncError()
-    )
-
-    print("Characters has been dropped!")
-end)
